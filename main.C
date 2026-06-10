2f3f43a3e7a176193045b533b31b505a17213ac2
deekshareddy719-ship-it <deekshareddy719@gmail.com>
2026-06-10 09:41:04 +0530

    Update main.C

--- main.C
+++ main.C
@@ -34,4 +34,29 @@ void displayCanvas()
 
 int isValid(int row,int col)
 {
-    return(row>=0 && row<ROWS && col
+    return(row>=0 && row<ROWS && col>=0 && col<COLS);
+}
+
+void drawRectangle(int row,int col,int height,int width,char ch)
+{
+    int i;
+    for(i=col;i<col+width && isValid(row,i) && isValid(row+height-1,i);i++)
+{
+      if(isValid(row,i))
+canvas[row][i]=ch;
+      if(isValid(row+height-1,i))canvas[row+height-1][i]=ch;
+}
+
+for(i=row;i<row + height && isValid(i,col + width - 1);i++)
+     {
+          if(isValid(i,col)) canvas[i][col+width-1]=ch;
+          if(isValid(i,col + width - 1))canvas[i][col+width-1]=ch;
+      }
+}
+
+void drawFilledRectangle(int row,int col,int height,int width,char ch)
+{
+   int i,j;
+   for(i=row;i<row+height && i<ROWS;i++)
+   {
+    
